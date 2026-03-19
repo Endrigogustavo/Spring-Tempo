7a9623fd07e41fbd6c13f13e6c2a22e1c5a7d9a0
Endrigogustavo <endrigogustavo2006@gmail.com>
2026-03-18 22:15:14 -0300

    fix: mongo files to use txt file were removed

--- spring/src/main/java/io/github/fatec/repository/adapter/TempoRepositoryAdapter.java
+++ spring/src/main/java/io/github/fatec/repository/adapter/TempoRepositoryAdapter.java
@@ -1,13 +1,12 @@
 package io.github.fatec.repository.adapter;
 
 import io.github.fatec.entity.Tempo;
-import io.github.fatec.repository.orm.TempoOrmMongo;
 
 public class TempoRepositoryAdapter {
     private TempoRepositoryAdapter() {
     }
 
-    public static Tempo castOrm(TempoOrmMongo orm) {
+    public static Tempo castOrm(Tempo orm) {
         return new Tempo(
                 orm.tempoZona(),
                 orm.dataHora(),
@@ -17,13 +16,7 @@ public class TempoRepositoryAdapter {
                 orm.unidadeVento());
     }
 
-    public static TempoOrmMongo castEntity(Tempo entity) {
-        return new TempoOrmMongo(
-                entity.tempoZona(),
-                entity.dataHora(),
-                entity.temperaturaDia(),
-                entity.unidadeTemperaturaDia(),
-                entity.vento(),
-                entity.unidadeVento());
+    public static Tempo castEntity(Tempo entity) {
+        return castOrm(entity);
     }
 }
